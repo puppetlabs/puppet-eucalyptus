dd2e203643ba6d560ab47024c63c2641252e1068
teyo <teyo@puppetlabs.com>
2011-07-14 17:31:55 -0700

    added inline documentation to the eucalyptus base clase

--- eucalyptus/manifests/init.pp
+++ eucalyptus/manifests/init.pp
@@ -1,3 +1,30 @@
+# This is the baseclass for Eucalpytus installs. This class sets up the yumrepo 
+# and for Eucalyptus version 2.0 installs dependencies. 
+#
+# == Parameters
+#
+# [*version*] The version of Eucalpytus we are installing. Defaults to 2.0.
+# [*reporul*] The uri for the package repo that is being used for this install. This is user configurable. 
+#
+# [*servers*]
+#   Description of servers class parameter.  e.g. "Specify one or more
+#   upstream ntp servers as an array."
+#
+# == Examples
+#
+# class { eucalyptus: version => '3.0' }  
+#
+#
+# == Authors
+#
+# Teyo Tyree <teyo@puppetlabs.com\>
+# David Kavanagh <david.kavanagh@eucalyptus.com\>
+#
+# == Copyright
+#
+# Copyright 2011 Eucalyptus INC under the Apache 2.0 license
+#
+
 class eucalyptus (
   $version = '2.0',
   $repourl = 'http://www.eucalyptussoftware.com/downloads/repo/eucalyptus/2.0.2/yum/centos/'
@@ -19,9 +46,9 @@ class eucalyptus (
                     'scsi-target-utils',
                     'httpd'] 
     }
-  }  
-  default: {
-    $packages = 'UNSET'
+    default: {
+      $packages = 'UNSET'
+    }
   }
   if $packages != 'UNSET' { 
     package { $packages: ensure => present }   
