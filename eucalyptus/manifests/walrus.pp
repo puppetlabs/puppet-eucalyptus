aa462b29227bddc91eaa9e9ca967bb531c6457e7
Tom Ellis <tom.ellis@eucalyptus.com>
2012-07-27 15:13:19 +0200

    use ipaddress fact instead of ec2_public_hostname

--- eucalyptus/manifests/walrus.pp
+++ eucalyptus/manifests/walrus.pp
@@ -4,7 +4,7 @@ class eucalyptus::walrus {
     ensure => present,
   }
   @@exec { "reg-walrus":
-    command => "/usr/sbin/euca_conf --no-rsync --register-walrus $ec2_public_hostname; exit 0",
+    command => "/usr/sbin/euca_conf --no-rsync --register-walrus $ipaddress; exit 0",
     unless => "/usr/sbin/euca_conf --list-walruses | tail -n+2",
     tag => "${cloud_name}",
   }
