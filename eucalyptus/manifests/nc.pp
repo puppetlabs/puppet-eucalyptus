324455a5b282f8a6e97599eaee7c3d7c86eab4bd
Tom Ellis <tom.ellis@eucalyptus.com>
2012-07-27 13:04:48 +0200

    move away from using ec2_public_hostname fact, node controllers use

--- eucalyptus/manifests/nc.pp
+++ eucalyptus/manifests/nc.pp
@@ -13,7 +13,7 @@ class eucalyptus::nc {
   Package[eucalyptus-nc] -> Eucalyptus_config<||> -> Service[eucalyptus-nc]
   Eucalyptus_config <||>
   @@exec { 'reg-nc':
-    command => "/usr/sbin/euca_conf --no-rsync --register-nodes $ec2_public_hostname; exit 0",
+    command => "/usr/sbin/euca_conf --no-rsync --register-nodes $ipaddress_br0; exit 0",
     tag => "${cloud_name}",
   }
   File <<|title == "${cloud_name}-cluster00-cc-cert"|>>
