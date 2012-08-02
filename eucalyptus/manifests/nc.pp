d73c91fb5c1b71aa7dce81158bd1ad80ecbb6290
Tom Ellis <tom.ellis@eucalyptus.com>
2012-08-02 17:47:45 +0200

    Update node registration options since 3.x has new options for not syncing keys

--- eucalyptus/manifests/nc.pp
+++ eucalyptus/manifests/nc.pp
@@ -13,7 +13,7 @@ class eucalyptus::nc ($cloud_name = "cloud1") {
   Package[eucalyptus-nc] -> Eucalyptus_config<||> -> Service[eucalyptus-nc]
   Eucalyptus_config <||>
   @@exec { 'reg-nc':
-    command => "/usr/sbin/euca_conf --no-rsync --register-nodes $ipaddress_br0; exit 0",
+    command => "/usr/sbin/euca_conf --no-rsync --no-sync --no-scp --register-nodes $ipaddress_br0; exit 0",
     tag => "${cloud_name}",
   }
   File <<|title == "${cloud_name}-cluster1-cc-cert"|>>
