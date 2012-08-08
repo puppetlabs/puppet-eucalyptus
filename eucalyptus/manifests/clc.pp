d5a008fbcd661b396faed19b588379f9f15c4f2a
Tom Ellis <tom.ellis@eucalyptus.com>
2012-08-08 12:23:09 +0200

    Add timeout=0 to initalize exec so it does not timeout when db takes longer to initialise, split class into separate stages to allow registration after install of pkgs, add euca.p12 to files collected from custom function

--- eucalyptus/manifests/clc.pp
+++ eucalyptus/manifests/clc.pp
@@ -3,49 +3,65 @@ class eucalyptus::clc ($cloud_name = "cloud1") {
 
   Class[eucalyptus] -> Class[eucalyptus::clc]
 
-  package { 'eucalyptus-cloud':
-    ensure => present,
+  class eucalyptus::clc_install {
+    package { 'eucalyptus-cloud':
+      ensure => present,
+    }
+    service { 'eucalyptus-cloud':
+      ensure => running,
+      enable => true,
+      require => Package['eucalyptus-cloud'],
+    }
   }
-  exec { 'init-db':
-    command => "/usr/sbin/euca_conf --initialize",
-    creates => "/var/lib/eucalyptus/db/data"
+  class eucalyptus::clc_config {
+    Class[eucalyptus::clc_install] -> Class[eucalyptus::clc_config]
+    exec { 'init-db':
+      command => "/usr/sbin/euca_conf --initialize",
+      creates => "/var/lib/eucalyptus/db/data",
+      timeout => "0",
+    }
+    @@file { "${cloud_name}-cluster1-nc-cert":
+      path => '/var/lib/eucalyptus/keys/node-cert.pem',
+      content => "$eucakeys_cluster1_node_cert",
+      tag => "${cloud_name}",
+    }
+    @@file { "${cloud_name}-cluster1-nc-pk":
+      path => '/var/lib/eucalyptus/keys/node-pk.pem',
+      content => "$eucakeys_cluster1_node_pk",
+      tag => "${cloud_name}",
+    }
+    @@file { "${cloud_name}-cluster1-cc-cert":
+      path => '/var/lib/eucalyptus/keys/cluster-cert.pem',
+      content => "$eucakeys_cluster1_cluster_cert",
+      tag => "${cloud_name}",
+    }
+    @@file { "${cloud_name}-cluster1-cc-pk":
+      path => '/var/lib/eucalyptus/keys/cluster-pk.pem',
+      content => "$eucakeys_cluster1_cluster_pk",
+      tag => "${cloud_name}",
+    }
+    @@file { "${cloud_name}-cloud-cert":
+      path => '/var/lib/eucalyptus/keys/cloud-cert.pem',
+      content => "$eucakeys_cloud_cert",
+      tag => "${cloud_name}",
+    }
+    @@file { "${cloud_name}-cloud-pk":
+      path => '/var/lib/eucalyptus/keys/cloud-pk.pem',
+      content => "$eucakeys_cloud_pk",
+      tag => "${cloud_name}",
+    }
+    @@file { "${cloud_name}-euca.p12":
+      path => '/var/lib/eucalyptus/keys/euca.p12',
+      content => "$eucakeys_euca_p12",
+      tag => "${cloud_name}",
+    }
+    Package[eucalyptus-cloud] -> Eucalyptus_config<||> -> Service[eucalyptus-cloud]
+    Eucalyptus_config <||>
   }
-  service { 'eucalyptus-cloud':
-    ensure => running,
-    enable => true,
-    require => [ Package['eucalyptus-cloud'], Exec['init-db'] ],
+  class eucalyptus::clc_reg {
+    Class[eucalyptus::clc_reg] -> Class[eucalyptus::clc_config]
+    Exec <<|tag == "$cloud_name"|>>
   }
-  @@file { "${cloud_name}-cluster1-nc-cert":
-    path => '/var/lib/eucalyptus/keys/node-cert.pem',
-    content => "$eucakeys_cluster1_node_cert",
-    tag => "${cloud_name}",
-  }
-  @@file { "${cloud_name}-cluster1-nc-pk":
-    path => '/var/lib/eucalyptus/keys/node-pk.pem',
-    content => "$eucakeys_cluster1_node_pk",
-    tag => "${cloud_name}",
-  }
-  @@file { "${cloud_name}-cluster1-cc-cert":
-    path => '/var/lib/eucalyptus/keys/cluster-cert.pem',
-    content => "$eucakeys_cluster1_cluster_cert",
-    tag => "${cloud_name}",
-  }
-  @@file { "${cloud_name}-cluster1-cc-pk":
-    path => '/var/lib/eucalyptus/keys/cluster-pk.pem',
-    content => "$eucakeys_cluster1_cluster_pk",
-    tag => "${cloud_name}",
-  }
-  @@file { "${cloud_name}-cloud-cert":
-    path => '/var/lib/eucalyptus/keys/cloud-cert.pem',
-    content => "$eucakeys_cloud_cert",
-    tag => "${cloud_name}",
-  }
-  @@file { "${cloud_name}-cloud-pk":
-    path => '/var/lib/eucalyptus/keys/cloud-pk.pem',
-    content => "$eucakeys_cloud_pk",
-    tag => "${cloud_name}",
-  }
-  Package[eucalyptus-cloud] -> Eucalyptus_config<||> -> Service[eucalyptus-cloud]
-  Eucalyptus_config <||>
-  Exec <<|tag == "$cloud_name"|>>
+
+  include eucalyptus::clc_install, eucalyptus::clc_config, eucalyptus::clc_reg
 }
