2939bc7a2146cab83ebc92d917de865831ab6189
Tom Ellis <tom.ellis@eucalyptus.com>
2012-08-08 12:29:46 +0200

    Add service resource to SC service. Move reg_sc command to updated syntax for 3.1. Distribute euca.p12 to SC from CLC.

--- eucalyptus/manifests/sc.pp
+++ eucalyptus/manifests/sc.pp
@@ -3,8 +3,15 @@ class eucalyptus::sc ($cloud_name = "cloud1") {
   package { 'eucalyptus-sc':
     ensure => present,
   }
+  service { 'eucalyptus-cloud':
+    ensure => running,
+    enable => true,
+    require => Package['eucalyptus-sc'],
+  } 
   @@exec { "reg_sc_${hostname}":
-    command => "/usr/sbin/euca_conf --no-rsync --no-scp --no-sync --register-sc cluster1 $ipaddress",
+    command => "/usr/sbin/euca_conf --no-rsync --no-scp --no-sync --register-sc --partition cluster1 --host $ipaddress --component sc_$hostname; exit 0",
     tag => "${cloud_name}",
   }
+  File <<|title == "${cloud_name}-euca.p12"|>>
+  Package[eucalyptus-sc] -> Eucalyptus_config<||> -> Service[eucalyptus-cloud]
 }
