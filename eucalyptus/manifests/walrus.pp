c5355f1b7d12b96074c5b29f8fcb7e245675f578
Tom Ellis <tom.ellis@eucalyptus.com>
2012-08-08 12:32:18 +0200

    Add service resource to walrus service. Update walrus registration command for 3.1. Distribute euca.p12 to walrus too

--- eucalyptus/manifests/walrus.pp
+++ eucalyptus/manifests/walrus.pp
@@ -3,9 +3,16 @@ class eucalyptus::walrus ($cloud_name = "cloud1") {
   package { 'eucalyptus-walrus':
     ensure => present,
   }
+  service { 'eucalyptus-cloud':
+    ensure => running,
+    enable => true,
+    require => Package['eucalyptus-walrus'],
+  } 
   @@exec { "reg_walrus_${hostname}":
-    command => "/usr/sbin/euca_conf --no-rsync --register-walrus --partition walrus --host $ipaddress --component $hostname; exit 0",
-    unless => "/usr/sbin/euca_conf --list-walruses | tail -n+2",
+    command => "/usr/sbin/euca_conf --no-rsync --no-scp --no-sync --register-walrus --partition walrus --host $ipaddress --component walrus_$hostname; exit 0",
+    #unless => "/usr/sbin/euca_conf --list-walruses | tail -n+2",
     tag => "${cloud_name}",
   }
+  File <<|title == "${cloud_name}-euca.p12"|>>
+  Package[eucalyptus-walrus] -> Eucalyptus_config<||> -> Service[eucalyptus-cloud]
 }
