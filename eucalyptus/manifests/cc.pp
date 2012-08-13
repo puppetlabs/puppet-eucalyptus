d8a2f0c2ffd55129828e9954e96020d34b4bb24f
Tom Ellis <tom.ellis@eucalyptus.com>
2012-08-13 12:47:10 +0200

    refactor cc.pp into three stages that depend on each other to get ordering correct

--- eucalyptus/manifests/cc.pp
+++ eucalyptus/manifests/cc.pp
@@ -4,25 +4,36 @@ class eucalyptus::cc ($cloud_name = "cloud1") {
 
   include eucalyptus::conf
 
-  package { 'eucalyptus-cc':
-    ensure => present,
+  class eucalyptus::cc_install {
+    package { 'eucalyptus-cc':
+      ensure => present,
+    }
+    service { 'eucalyptus-cc':
+      ensure => running,
+      enable => true,
+    }
   }
-  service { 'eucalyptus-cc':
-    ensure => running,
-    enable => true,
+
+  class eucalyptus::cc_config {
+    File <<|title == "${cloud_name}-cloud-cert"|>>
+    File <<|title == "${cloud_name}-cloud-pk"|>>
+    File <<|title == "${cloud_name}-cluster1-cc-cert"|>>
+    File <<|title == "${cloud_name}-cluster1-cc-pk"|>>
+    File <<|title == "${cloud_name}-cluster1-nc-cert"|>>
+    File <<|title == "${cloud_name}-cluster1-nc-pk"|>>
+    Package[eucalyptus-cc] -> Eucalyptus_config<||> -> Service[eucalyptus-cc]
+    Eucalyptus_config <||>
   }
-  Package[eucalyptus-cc] -> Eucalyptus_config<||> -> Service[eucalyptus-cc]
-  Eucalyptus_config <||>
-  @@exec { "reg_cc_${hostname}":
-    command => "/usr/sbin/euca_conf --no-rsync --no-scp --no-sync --register-cluster --partition cluster1 --host $ipaddress --component cc_$hostname; exit 0",
-    tag => "${cloud_name}",
+
+  class eucalyptus::cc_reg {
+    Class[eucalyptus::cc_reg] -> Class[eucalyptus::cc_config]
+    @@exec { "reg_cc_${hostname}":
+      command => "/usr/sbin/euca_conf --no-rsync --no-scp --no-sync --register-cluster --partition cluster1 --host $ipaddress --component cc_$hostname; exit 0",
+      tag => "${cloud_name}",
+    }
+    # Register NC's from CC
+    Exec <<|tag == "${cloud_name}_reg_nc"|>>
   }
-  File <<|title == "${cloud_name}-cloud-cert"|>>
-  File <<|title == "${cloud_name}-cloud-pk"|>>
-  File <<|title == "${cloud_name}-cluster1-cc-cert"|>>
-  File <<|title == "${cloud_name}-cluster1-cc-pk"|>>
-  File <<|title == "${cloud_name}-cluster1-nc-cert"|>>
-  File <<|title == "${cloud_name}-cluster1-nc-pk"|>>
-  # Register NC's from CC
-  Exec <<|tag == "${cloud_name}_reg_nc"|>>
+
+  include eucalyptus::cc_install, eucalyptus::cc_config, eucalyptus::cc_reg
 }
