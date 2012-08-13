3ca54fbd3813af95f80f849b59b782d4014adb2a
Olivier Renault <orenault@beefy.monolive.org>
2012-08-13 10:01:57 +0200

    Setting repository configuration from repo.pp, it should work Red Hat based OS as well as Ubuntu Modified security.pp to be only run on Red Hat based OS Corrected a typo in file ntp.conf Removed files eucalyptus.repo

--- eucalyptus/manifests/security.pp
+++ eucalyptus/manifests/security.pp
@@ -1,8 +1,23 @@
+# Disable selinux + firewall for RedHat based OS
+
 class eucalyptus::security {
-  exec { "/bin/sed --in-place=.bak 's/enforcing/disabled/g' /etc/selinux/config":
-  }
-  exec { "/bin/sed --in-place=.bak 's/enforcing/disabled/g' /etc/sysconfig/system-config-firewall":
-  }
-  exec { "/bin/grep -v 'reject-with' /etc/sysconfig/iptables >/tmp/iptables; /bin/cp /tmp/iptables /etc/sysconfig":
+  case $operatingsystem {
+    centos, rhel : {
+      if $selinux == 'true' {
+        exec { "/bin/sed --in-place=.bak 's/enforcing/disabled/g' /etc/selinux/config":
+        }
+      }
+      exec {"disable-firewall":
+        command => "/bin/sed --in-place=.bak 's/enabled/disabled/g' /etc/sysconfig/system-config-firewall",
+        unless  => "/usr/bin/test -f /etc/sysconfig/system-config-firewall.bak",
+      }
+      service {"iptables":
+        enable  => false,
+      }
+      exec { "/bin/grep -v 'reject-with' /etc/sysconfig/iptables >/tmp/iptables; /bin/cp /tmp/iptables /etc/sysconfig":
+        unless  => "/usr/bin/test -f /tmp/iptables",
+      }
+    }
   }
 }
+
