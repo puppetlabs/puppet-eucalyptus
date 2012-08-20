7c5d589457d79f7f6e9a6f904743ed419bfb1fc3
Tom Ellis <tom.ellis@eucalyptus.com>
2012-08-20 16:00:09 +0200

    Convert custom eucakeys fact to collect all keys in any directory below /var/lib/eucalyptus/keys

--- eucalyptus/lib/facter/eucakeys.rb
+++ eucalyptus/lib/facter/eucakeys.rb
@@ -1,5 +1,11 @@
+# eucakeys.rb
+# Grab all eucalyptus key files and export them as facts so we can use them within the Eucalyptus module
+
 require 'facter'
+
 eucakey_dir = '/var/lib/eucalyptus/keys'
+
+# Get all keys in the top level eucakey_dir
 if File.directory?(eucakey_dir)
   keyfiles = Dir.entries(eucakey_dir)
   keyfiles.each do |name|
@@ -17,22 +23,21 @@ if File.directory?(eucakey_dir)
         end
       end
     end
-
   end
 end
-## this is TEMPORARY!
-# the clustername is a sub dir that should be dynamic. keys for a cc and nc need
-# to be fetched from the appropriate cluster dir
-# for now, we're hard-coding a cluster name
-if File.directory?(eucakey_dir+'/cluster1')
-  keyfiles = Dir.entries(eucakey_dir+'/cluster1')
-  keyfiles.each do |name|
-    if name.match(/\.pem/)
-      Facter.add("eucakeys_cluster1_#{name.sub('.pem','').sub('-','_')}") do
-        setcode do
-          File.read("#{eucakey_dir}/cluster1/#{name}")
+
+# Check if entries in eucakey_dir are directories, if they are return them minus the . and .. entries
+dir_contents = Dir.entries(eucakey_dir).select {|entry| File.directory? File.join(eucakey_dir,entry) and !(entry =='.' || entry == '..') }
+# For each cluster directory, grab all pem files and set as facts making sure all fact names use underscores
+dir_contents.each do |clustername|
+ keyfiles = Dir.entries(eucakey_dir+"/"+clustername)
+ keyfiles.each do |keyname|
+   if keyname.match(/\.pem/)
+     Facter.add("eucakeys_" + clustername + "_#{keyname.sub('.pem','').sub('-','_')}") do
+       setcode do
+          File.read("#{eucakey_dir}/#{clustername}/#{keyname}")
         end
-      end
-    end
-  end
+     end
+   end
+ end
 end
