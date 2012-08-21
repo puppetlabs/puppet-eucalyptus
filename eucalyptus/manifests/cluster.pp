ceb04073048dce2a2ca7b43163c373855045ed2b
Tom Ellis <tom.ellis@eucalyptus.com>
2012-08-21 14:31:32 +0200

    Add inline_template magic to correctly interpolate  into  fact

--- eucalyptus/manifests/cluster.pp
+++ eucalyptus/manifests/cluster.pp
@@ -1,33 +1,39 @@
 # Resource for the CLC, so we can iterate over cluster names and call this more than once
+#  
+#  - interpolating $cluster_name into $eucakeys_$cluster_name_node_cert (etc) with inline_templates
   define eucalyptus::cluster($cluster_name) {
     # One of these for each cluster
+    $node_cert = "eucakeys_${cluster_name}_node_cert"
     @@file { "${cloud_name}-${cluster_name}-nc-cert":
       path => '/var/lib/eucalyptus/keys/node-cert.pem',
-      content => "$eucakeys_${cluster_name}_node_cert",
+      content => inline_template("<%= scope.lookupvar(node_cert) %>"),
       owner  => 'eucalyptus',
       group  => 'eucalyptus',
       mode   => '0700',
       tag => "${cloud_name}",
     }
+    $nc_pk = "eucakeys_${cluster_name}_node_pk"
     @@file { "${cloud_name}-${cluster_name}-nc-pk":
       path => '/var/lib/eucalyptus/keys/node-pk.pem',
-      content => "$eucakeys_${cluster_name}_node_pk",
+      content => inline_template("<%= scope.lookupvar(nc_pk) %>"),
       owner  => 'eucalyptus',
       group  => 'eucalyptus',
       mode   => '0700',
       tag => "${cloud_name}",
     }
+    $cc_cert = "eucakeys_${cluster_name}_cluster_cert"
     @@file { "${cloud_name}-${cluster_name}-cc-cert":
       path => '/var/lib/eucalyptus/keys/cluster-cert.pem',
-      content => "${eucakeys}_${cluster_name}_cluster_cert",
+      content => inline_template("<%= scope.lookupvar(cc_cert) %>"),
       owner  => 'eucalyptus',
       group  => 'eucalyptus',
       mode   => '0700',
       tag => "${cloud_name}",
     }
+    $cc_pk = "eucakeys_${cluster_name}_cluster_pk"
     @@file { "${cloud_name}-${cluster_name}-cc-pk":
       path => '/var/lib/eucalyptus/keys/cluster-pk.pem',
-      content => "${eucakeys}_${cluster_name}_cluster_pk",
+      content => inline_template("<%= scope.lookupvar(cc_pk) %>"),
       owner  => 'eucalyptus',
       group  => 'eucalyptus',
       mode   => '0700',
