ebb402896a0d9727538a3db359636bff73da6bea
Tom Ellis <tom.ellis@eucalyptus.com>
2012-10-10 05:47:37 -0400

    fix spacing

--- eucalyptus/manifests/clc.pp
+++ eucalyptus/manifests/clc.pp
@@ -24,28 +24,28 @@ class eucalyptus::clc ($cloud_name = "cloud1") {
 
     # Cloud-wide
     @@file { "${cloud_name}_cloud_cert":
-      path => '/var/lib/eucalyptus/keys/cloud-cert.pem',
+      path    => '/var/lib/eucalyptus/keys/cloud-cert.pem',
       content => "$eucakeys_cloud_cert",
-      owner  => 'eucalyptus',
-      group  => 'eucalyptus',
-      mode   => '0700',
-      tag => "${cloud_name}",
+      owner   => 'eucalyptus',
+      group   => 'eucalyptus',
+      mode    => '0700',
+      tag     => "${cloud_name}",
     }
     @@file { "${cloud_name}_cloud_pk":
-      path => '/var/lib/eucalyptus/keys/cloud-pk.pem',
+      path    => '/var/lib/eucalyptus/keys/cloud-pk.pem',
       content => "$eucakeys_cloud_pk",
-      owner  => 'eucalyptus',
-      group  => 'eucalyptus',
-      mode   => '0700',
-      tag => "${cloud_name}",
+      owner   => 'eucalyptus',
+      group   => 'eucalyptus',
+      mode    => '0700',
+      tag     => "${cloud_name}",
     }
     @@file { "${cloud_name}_euca.p12":
-      path => '/var/lib/eucalyptus/keys/euca.p12',
+      path    => '/var/lib/eucalyptus/keys/euca.p12',
       content => "$eucakeys_euca_p12",
-      owner  => 'eucalyptus',
-      group  => 'eucalyptus',
-      mode   => '0700',
-      tag => "${cloud_name}",
+      owner   => 'eucalyptus',
+      group   => 'eucalyptus',
+      mode    => '0700',
+      tag     => "${cloud_name}",
     }
     
     Eucalyptus_config <||>
