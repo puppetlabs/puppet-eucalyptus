fed6d90a62651d4ad6b09fb5db5ff09b65db207e
Tom Ellis <tom.ellis@eucalyptus.com>
2012-10-10 17:37:26 +0200

    Add vtun support to custom fact, align spacing in cluster.pp and add vtunpass

--- eucalyptus/manifests/cluster.pp
+++ eucalyptus/manifests/cluster.pp
@@ -5,38 +5,47 @@
     # One of these for each cluster
     $node_cert = "eucakeys_${cluster_name}_node_cert"
     @@file { "${cloud_name}_${cluster_name}_node_cert":
-      path => '/var/lib/eucalyptus/keys/node-cert.pem',
+      path    => '/var/lib/eucalyptus/keys/node-cert.pem',
       content => inline_template("<%= scope.lookupvar(node_cert) %>"),
-      owner  => 'eucalyptus',
-      group  => 'eucalyptus',
-      mode   => '0700',
-      tag => "${cloud_name}",
+      owner   => 'eucalyptus',
+      group   => 'eucalyptus',
+      mode    => '0700',
+      tag     => "${cloud_name}",
     }
     $nc_pk = "eucakeys_${cluster_name}_node_pk"
     @@file { "${cloud_name}_${cluster_name}_node_pk":
-      path => '/var/lib/eucalyptus/keys/node-pk.pem',
+      path    => '/var/lib/eucalyptus/keys/node-pk.pem',
       content => inline_template("<%= scope.lookupvar(nc_pk) %>"),
-      owner  => 'eucalyptus',
-      group  => 'eucalyptus',
-      mode   => '0700',
-      tag => "${cloud_name}",
+      owner   => 'eucalyptus',
+      group   => 'eucalyptus',
+      mode    => '0700',
+      tag     => "${cloud_name}",
     }
     $cc_cert = "eucakeys_${cluster_name}_cluster_cert"
     @@file { "${cloud_name}_${cluster_name}_cluster_cert":
-      path => '/var/lib/eucalyptus/keys/cluster-cert.pem',
+      path    => '/var/lib/eucalyptus/keys/cluster-cert.pem',
       content => inline_template("<%= scope.lookupvar(cc_cert) %>"),
-      owner  => 'eucalyptus',
-      group  => 'eucalyptus',
-      mode   => '0700',
-      tag => "${cloud_name}",
+      owner   => 'eucalyptus',
+      group   => 'eucalyptus',
+      mode    => '0700',
+      tag     => "${cloud_name}",
     }
     $cc_pk = "eucakeys_${cluster_name}_cluster_pk"
     @@file { "${cloud_name}_${cluster_name}_cluster_pk":
-      path => '/var/lib/eucalyptus/keys/cluster-pk.pem',
+      path    => '/var/lib/eucalyptus/keys/cluster-pk.pem',
       content => inline_template("<%= scope.lookupvar(cc_pk) %>"),
-      owner  => 'eucalyptus',
-      group  => 'eucalyptus',
-      mode   => '0700',
-      tag => "${cloud_name}",
+      owner   => 'eucalyptus',
+      group   => 'eucalyptus',
+      mode    => '0700',
+      tag     => "${cloud_name}",
+    }
+    $cc_vtunpass = "eucakeys_${cluster_name}_vtunpass"
+    @@file { "${cloud_name}_${cluster_name}_vtunpass":
+      path    => '/var/lib/eucalyptus/keys/vtunpass',
+      content => inline_template("<%= scope.lookupvar(cc_vtunpass) %>"),
+      owner   => 'eucalyptus',
+      group   => 'eucalyptus',
+      mode    => '0700',
+      tag     => "${cloud_name}",
     }
   }
